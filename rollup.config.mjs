547d1356961d1c8111afd0b067c293db9ee0428c
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2026-02-27 15:22:49 -0500

    published to npm

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,25 +1,40 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
-const plugins = [resolve(), commonjs()];
+const plugins = [
+    resolve({
+        browser: true,
+        preferBuiltins: false,
+    }),
+    commonjs(),
+];
 
-export default [
-	{
-		input: "src/js/loadScores.mjs",
-		output: {
-			file: "dist/loadScores.bundle.mjs",
-			format: "es",
-			sourcemap: true,
-		},
-		plugins,
-	},
-	{
-		input: "src/js/loadTraits.js",
-		output: {
-			file: "dist/loadTraits.bundle.mjs",
-			format: "es",
-			sourcemap: true,
-		},
-		plugins,
-	},
+
+export default [{
+        input: "src/js/loadScores.mjs",
+        output: {
+            file: "dist/loadScores.bundle.mjs",
+            format: "es",
+            sourcemap: true,
+        },
+        plugins,
+    },
+    {
+        input: "src/js/loadTraits.js",
+        output: {
+            file: "dist/loadTraits.bundle.mjs",
+            format: "es",
+            sourcemap: true,
+        },
+        plugins
+    },
+    {
+        input: "sdk.js",
+        output: {
+            file: "dist/sdk.mjs",
+            format: "es",
+            sourcemap: true,
+        },
+        plugins,
+    },
 ];
