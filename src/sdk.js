cea3ff26717fbac12664b2bb0f9ba5a4b83ebf87
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2026-02-27 14:05:36 -0500

    added sdk

---
+++ src/sdk.js
@@ -0,0 +1,5 @@
+import { loadScores } from "./js/loadScores";       
+export { loadScores } from "./js/loadScores"; // re-export for external use
+
+// Example usage of loadScores() in an async 
+// function, e.g. on page load or button click    
